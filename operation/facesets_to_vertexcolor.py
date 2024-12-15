e18fdbd6c12f8514c7dcd541669a456c58593c3c
ozasuke33 <ozasuke33@gmail.com>
2024-12-15 09:33:20 +0900

    update

--- operation/facesets_to_vertexcolor.py
+++ operation/facesets_to_vertexcolor.py
@@ -3,6 +3,7 @@ import bmesh
 
 import random
 
+
 class OBJECT_OT_facesets_to_vertexcolor(bpy.types.Operator):
     bl_idname = "gaops.facesets_to_vertexcolor"
     bl_label = "Face Sets to Vertex Color"
@@ -10,43 +11,64 @@ class OBJECT_OT_facesets_to_vertexcolor(bpy.types.Operator):
 
     def execute(self, context):
         obj_as_mesh = []
+        facesets_colors = []
 
         for obj in context.selected_objects:
             if obj.type == "MESH":
                 obj_as_mesh.append(obj)
+
+        # bpy.ops.object.mode_set(mode="VERTEX_PAINT")
 
         for obj in obj_as_mesh:
             facesets = {}
             bm = bmesh.new()
             bm.from_mesh(obj.data)
 
-            layer_facesets = bm.faces.layers.int.get('.sculpt_face_set')
+            layer_facesets = bm.faces.layers.int.get(".sculpt_face_set")
             if layer_facesets == None:
                 break
 
-            if len(bm.loops.layers.color) == 0:
-                bm.loops.layers.color.new("ID Map")
-            layer_vertexcolor = bm.loops.layers.color[0]
-            for c in bm.loops.layers.color:
-                if c.name == "ID Map":
-                    layer_vertexcolor = c
+            layer_vertexcolor = bm.loops.layers.color[
+                obj.data.attributes.active_color_index
+            ]
+            bpy.ops.geometry.color_attribute_render_set(name=layer_vertexcolor.name)
 
             for face in bm.faces:
                 fs = face[layer_facesets]
                 vertexcolor = [0, 0, 0]
-                
+
                 if fs not in facesets:
-                    vertexcolor = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+                    vertexcolor = [
+                        random.randint(0, 255),
+                        random.randint(0, 255),
+                        random.randint(0, 255),
+                    ]
                     while True:
                         roll_dice = False
                         for _, color in facesets.items():
-                            if color[0] == vertexcolor[0] and color[1] == vertexcolor[1] and color[2] == vertexcolor[2]:
+                            if (
+                                color[0] == vertexcolor[0]
+                                and color[1] == vertexcolor[1]
+                                and color[2] == vertexcolor[2]
+                            ):
+                                roll_dice = True
+                        for color in facesets_colors:
+                            if (
+                                color[0] == vertexcolor[0]
+                                and color[1] == vertexcolor[1]
+                                and color[2] == vertexcolor[2]
+                            ):
                                 roll_dice = True
                         if roll_dice:
-                            vertexcolor = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+                            vertexcolor = [
+                                random.randint(0, 255),
+                                random.randint(0, 255),
+                                random.randint(0, 255),
+                            ]
                         else:
                             break
                     facesets[fs] = vertexcolor
+                    facesets_colors.append(vertexcolor)
                 if fs in facesets:
                     vertexcolor = facesets[fs]
 
