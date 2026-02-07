7a7ad9f4782941eba9efd0273c1695f767cd1131
ozasuke33 <ozasuke33@gmail.com>
2026-02-07 19:56:52 +0900

    トグルになってなかった

--- operation/toggle_backface_culling.py
+++ operation/toggle_backface_culling.py
@@ -7,10 +7,6 @@ class OBJECT_OT_toggle_backface_culling(bpy.types.Operator):
     bl_label = "Toggle backface culling"
     bl_options = {"REGISTER", "UNDO"}
 
-    use_backface_culling: bpy.props.BoolProperty(
-        name="Use backface culling", default=True
-    )
-
     @classmethod
     def poll(cls, context):
         if context.mode != "OBJECT":
@@ -26,6 +22,8 @@ class OBJECT_OT_toggle_backface_culling(bpy.types.Operator):
         for obj in obj_as_mesh:
             for slot in obj.material_slots:
                 if slot.material:
-                    slot.material.use_backface_culling = self.use_backface_culling
+                    slot.material.use_backface_culling = (
+                        not slot.material.use_backface_culling
+                    )
 
         return {"FINISHED"}
